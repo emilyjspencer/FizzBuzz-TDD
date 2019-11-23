36093ff8b1d20160a95615fdfdd9eec249362003
lajoiedevivre89 <emilyspencer890@hotmail.co.uk>
2019-11-23 16:20:45 +0000

    Initializes RSpec

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,18 @@
+# Tests for FizzBuzz
+
+require 'fizzbuzz'
+
+describe FizzBuzz do
+  it 'returns "Fizz" if passed 3' do
+  expect(fizzbuzzing(3)).to eq "Fizz"
+  end
+  
+
+
+    
+    
+    
+    
+    
+    
+end
